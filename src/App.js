399680c6efb7c8833e16129d2cc586729534ed42
kyungho <akh1216@gmail.com>
2022-07-15 10:48:57 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function App() {
   const name = 'react';
-  return <div>{name==='react'&&<h1>this is react</h1>}</div>;
+  const style ={
+    //background-color is backgroundColor와 같이 - 가 사라지고 카멜 표기법으로 적성된다.
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWidth: 'bold',
+    padding: 16
+  };
+  return <div style={style} >{name}</div>
 }
 
 export default App;
